abcd29b8a38526c78a6707fa5407d0d2e15bde3c
saarahabdel <saarahabdel15@gmail.com>
2023-05-05 08:47:55 +0200

    Exported data.js

--- book-connect-main/data.js
+++ book-connect-main/data.js
@@ -1,7 +1,7 @@
 
 BOOKS_PER_PAGE = 36;
 
-authors = {
+export const authors = {
   "194e164b-9365-4358-b44a-f28a93cc528f": "Steven D. Levitt",
   "76e8065c-fd7a-4a8b-a8ea-6105a47d0781": "Stephen J. Dubner",
   "6b092ae7-283c-45db-80f1-f0cc7e0d4921": "Bill Bryson",
