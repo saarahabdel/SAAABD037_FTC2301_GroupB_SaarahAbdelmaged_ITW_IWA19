cdd25003c5e91ba782ae60d61171a373b67800ca
saarahabdel <saarahabdel15@gmail.com>
2023-05-05 09:40:22 +0200

    Edited

--- book-connect-main/data.js
+++ book-connect-main/data.js
@@ -1,5 +1,5 @@
 // exported this information
-export const BOOKS_PER_PAGE = 36;
+export const BOOKS_PER_PAGE = 36; // maximum number of books shown per page
 
 export const authors = {
   "194e164b-9365-4358-b44a-f28a93cc528f": "Steven D. Levitt",
@@ -91,7 +91,7 @@ export const authors = {
   "37018341-31f4-4ffa-8755-a49979c218dd": "Suzanne Collins"
 }
 
-genres = {
+export const genres = {
   "a4f80b3e-3e96-4266-b729-e09b71793182": "Economics",
   "6dd5bb6e-0172-4d6e-aa18-26f00954dd7a": "Non-fiction",
   "5439a895-20a8-421a-981d-43f99b521cb5": "Business",
@@ -162,7 +162,7 @@ genres = {
   "64c4197d-5f8e-4bff-b440-6d19bc591fd9": "Magic"
 }
 
-books = [
+export const books = [
   {
     "id": "760b3450-9c86-42d0-8eff-e793bf823756",
     "genres": [
