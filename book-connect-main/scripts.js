a4c3913bcb636d57a194aa0df3c098e8e3ffa3e0
saarahabdel <saarahabdel15@gmail.com>
2023-05-10 19:46:10 +0200

    Edited

--- book-connect-main/scripts.js
+++ book-connect-main/scripts.js
@@ -133,50 +133,7 @@ moreButton.innerHTML = /* HTML */
 
 
 
-// BOOK SUMMARIES
-
-// When listItems is clicked, it shows a modal by invoking showModal() on dataListActive.
-listItems.addEventListener('click', (event) => {
-    listActive.showModal()
-    let pathArray = Array.from(event.path || event.composedPath())
-    let active;
-  
-    for (const node of pathArray) {
-      if (active) break;
-      const id = node?.dataset?.preview
-      
-      for (const singleBook of books) {
-        if (singleBook.id === id) {
-          active = singleBook
-          break;
-        }
-      }
-    }
-  
-    if (!active) return;
-    listImage.src = active.image;
-    listBlur.src = active.image;
-    listTitle.textContent = active.title; 
-    listSubtitle.textContent = `${authors[active.author]} (${new Date(active.published).getFullYear()})`
-    listDescription.textContent = active.description;
-})
-
-
-//When listClose is clicked, it closes the modal by invoking close() on dataListActive.
-listClose.addEventListener('click', () => {
-    listActive.close()
-})
-
-
-/**
- * GENRES AND AUTHORS
- * This code creates a document fragment for each 'genres' and 'authors'.
- * It sets the value of the option to "any" and the inner text to "All Genres" and "All Authors" 
- * respectively. It then loops through an object and creates an option element for each entry, 
- * setting the value to the entry's key and the inner text to its value. 
- * These option elements are added to the fragment, and the fragment is then 
- * appended to a searchGenres element and searchAuthors element.
- */
+// GENRES AND AUTHORS DROPDOWN
 
 //When searcButton is clicked, it shows a modal by invoking showModal() on dataSearchOverlay
 searchButton.addEventListener('click', () => {
@@ -189,105 +146,47 @@ searchCancel.addEventListener('click', () => {
     searchOverlay.close()
 })
 
+// a document fragment for 'genres'
 const genresFragment = document.createDocumentFragment()
 const genreElement = document.createElement('option')
-genreElement.value = 'any'
-genreElement.innerText = 'All Genres'
+genreElement.value = 'any'                             // sets the value of the option to "any"
+genreElement.innerText = 'All Genres'                  // sets inner text to "All Genres"
 genresFragment.appendChild(genreElement)
 
+// loops through an object and creates an option element for each entry, setting the value to the entry's key and the inner text to its value
 for (const [id] of Object.entries(genres)) {
     const genreElement = document.createElement('option')
     genreElement.value = id
     genreElement.innerText = genres[id]
+    // option elements are added to the fragment
     genresFragment.appendChild(genreElement)
 }
 
+// fragment is then appended to a searchGenres element
 searchGenres.appendChild(genresFragment)
 
+// a document fragment for 'authors'
 const authorsFragment = document.createDocumentFragment()
 const authorsElement = document.createElement('option')
-authorsElement.value = 'any'
-authorsElement.innerText = 'All Authors'
+authorsElement.value = 'any'                             // sets the value of the option to "any"
+authorsElement.innerText = 'All Authors'                 // sets inner text to "All Authors"
 authorsFragment.appendChild(authorsElement)
 
+// loops through an object and creates an option element for each entry, setting the value to the entry's key and the inner text to its value
 for (const [id] of Object.entries(authors)) {
     const authorsElement = document.createElement('option')
     authorsElement.value = id
     authorsElement.innerText = authors[id]
+    // option elements are added to the fragment
     authorsFragment.appendChild(authorsElement)
 }
 
+// fragment is then appended to a searchAuthors element
 searchAuthors.appendChild(authorsFragment)
 
-
-/**
- * FILTER BOOKS BY TITLE, GENRE AND AUTHOR
- * This code sets an event listener for a search form element, searchForm. 
- * When the form is submitted, the event listener prevents the default form 
- * submission behavior and instead executes a search using the provided form data.
- */
-
-searchForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-
-    /**The form data is obtained using FormData and converted into an object using Object.fromEntries. 
-     * The search is then performed on an array of books, with the search criteria based on the user's 
-     * form input.
-     */
-    const formData = new FormData(event.target);
-    const filters = Object.fromEntries(formData);
-    const result = [];
-  
-    for (let x = 0; x < books.length; x++) {
-      const titleState = filters.title.trim() && books[x].title.toLowerCase().includes(filters.title.toLowerCase());
-      const authorState = filters.author !== 'any' && books[x].author.includes(filters.author);
-      const genreState = filters.genre !== 'any' && books[x].genres.includes(filters.genre);
-  
-      if (titleState && authorState && genreState) {
-        result.push(books[x]);
-      }
-    }
-  
-    /**
-     * If the search returns results, they are displayed using a function called createPreviewsFragment, 
-     * and the result count is displayed in a button. If there are no results and the user has provided 
-     * search criteria, a message is displayed indicating that there were no results.
-     */
-    if (result.length > 0) {
-      const resultFragment = createPreview(result);
-      listItems.replaceChildren(resultFragment);
-      moreButton.innerHTML = /* html */ `
-        <span>Show more</span>
-        <span class="list__remaining"> (0)</span>
-      `;
-      moreButton.disabled = true;
-    //   showPreview();
-    } else if (filters.title.trim() && filters.author !== 'any' && filters.genre !== 'any') {
-      const firstElementChild = listMessage;
-      listItems.innerHTML = '';
-      listItems.replaceChildren(firstElementChild);
-      listMessage.style.display = 'block';
-      moreButton.innerHTML = /* html */ `
-        <span>Show more</span>
-        <span class="list__remaining"> (0)</span>
-      `;
-      moreButton.disabled = true;
-    }
-  
-    searchOverlay.close();
-    searchForm.reset();
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-})
   
 
-
-/** 
- * THEME SELECT
- * This code sets up event listeners and handles form submissions for a 
- * data settings overlay. When the header for the overlay is clicked, the 
- * overlay is shown. When the cancel button is clicked, the overlay is closed.
- * 
- */
+// DAY / NIGHT OPTION
 
 settingsButton.addEventListener('click', () => {
     settingsOverlay.showModal()
@@ -305,11 +204,9 @@ const css = {
 
 //The value of the settingsTheme input is determined based on whether the user's preferred color scheme is dark or not.
 settingsTheme.value = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'night' : 'day'
-let v = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'night' : 'day'
 
 /**
- * This code sets up for a submit event listener. When the form is submitted, the selected 
- * object is created by converting the form data to an object using Object.fromEntries(). 
+ * When the form is submitted, the selected object is created by converting the form data to an object using Object.fromEntries(). 
  * Depending on the theme selected, the --color-light and --color-dark CSS variables are 
  * updated with the corresponding light and dark color values from the css object
  */
@@ -328,4 +225,5 @@ settingsForm.addEventListener('submit', (event) => {
 
     settingsOverlay.close()
 })
+
 
